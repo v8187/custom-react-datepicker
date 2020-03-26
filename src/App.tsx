e8d1e0cd89a0caaacc5051d98b504943cfd9a518
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2020-03-27 00:04:35 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
+import './App.css';
+
+export default class App extends Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      selected: new Date()
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DatePicker
+          selected={this.state.selected}
+          monthsShown={2}
+          onChange={(evt) => this.onChange(evt)} />
+      </div>
+    );
+  }
+
+  onChange = (date: Date | null) => {
+    console.log('onChange::date', date);
+    this.setState({ selected: date });
+  }
+}
