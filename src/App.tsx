85d2e10f0e29933533d414543434e95697dcb6b5
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2020-03-28 13:53:22 +0530

    unable resolve issues in github src code tring changing npm module src...

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
-// import DatePicker from 'react-datepicker';
-import DatePicker from './react-datepicker'
+// import DatePicker from 'react-datepicker/es';
+// import DatePicker from './react-datepicker';
+import DatePicker from './es';
 
-import './react-datepicker/react-datepicker.css';
+import './react-datepicker.css';
 import './App.css';
 
-export default class App extends Component<any, any> {
+interface state {
+  selected: Date | null,
+  selectedMultiple: Date[],
+}
+
+export default class App extends Component<any, state> {
 
   constructor(props: any) {
     super(props);
 
     this.state = {
-      selected: new Date()
+      selected: new Date(),
+      selectedMultiple: [new Date()]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <DatePicker
+        <div><DatePicker
           selected={this.state.selected}
+          onChange={(evt) => this.handleSelected(evt)} /></div>
+
+        <div><DatePicker
+          selectedMultiple={this.state.selectedMultiple}
           monthsShown={2}
-          onChange={(evt) => this.onChange(evt)} />
+          selectNDates={true}
+          onChange={(evt) => this.handleSelectedMultiple(evt)} /></div>
       </div>
     );
   }
 
-  onChange = (date: Date | null) => {
-    console.log('onChange::date', date);
-    this.setState({ selected: date });
+  handleSelected = (date: Date[] | Date | null) => {
+    console.log('handleSelected::date', date);
+    this.setState({ selected: Array.isArray(date) ? date[0] : date });
+  }
+
+  handleSelectedMultiple = (dates: Date[] | Date | null) => {
+    console.log('handleSelectedMultiple::dates', dates);
+    this.setState({ selectedMultiple: Array.isArray(dates) ? dates : dates !== null ? [dates] : [] });
   }
 }
